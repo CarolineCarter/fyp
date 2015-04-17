7d5f328bec935d64a9e08f9c7b30423d49cb56c6
CarolineCarter <carolinecarter2@gmail.com>
2015-04-17 10:04:14 -0700

    tests

--- web_service/app/cpyserver.py
+++ web_service/app/cpyserver.py
@@ -2,28 +2,32 @@ from app import app
 
 # Import CherryPy
 import cherrypy
-import simplejson
+from simplejson import JSONEncoder 
+import json 
 
 if __name__ == '__main__':
 
     
     cherrypy.tree.graft(app, "/add")
-    cherrypy.response.headers["Access-Control-Allow-Origin"] = "172.17.0.17:80"
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "172.17.0.4:80"
     cherrypy.response.headers["Access-Control-Allow-Methods"] = "POST", "GET"
-    cherrypy.response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Type"
+    cherrypy.response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    cherrypy.response.headers["Allow"] = "POST"
+    #input_json = cherrypy.request.json
     cherrypy.response.headers["Content-Type"] = "application/json"
-
+    
+    
     # Unsubscribe the default server
     cherrypy.server.unsubscribe()
 
-    # Instantiate a new server object
+    
     server = cherrypy._cpserver.Server()
  
-    # Configure the server object
+    
     server.socket_host = "0.0.0.0"
     server.socket_port = 8000
     server.thread_pool = 30
-    '''
+     
     config = {
         'global': {
             'server.socket_host':'0.0.0.0',
@@ -35,7 +39,7 @@ if __name__ == '__main__':
             'tools.CORS.on': True
         }
     }
-    '''
+   
     # For SSL Support
     # server.ssl_module            = 'pyopenssl'
     # server.ssl_certificate       = 'ssl/certificate.crt'
@@ -44,7 +48,7 @@ if __name__ == '__main__':
 
     # Subscribe this server
     server.subscribe()
-
+    
     
 
     cherrypy.engine.start()
